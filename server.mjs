8513f90626561c24d3653566cff4e874f23ce2d5
DF64bit <96969533+DF64bit@users.noreply.github.com>
2023-06-17 10:31:51 +0000

    server.mjsをもう少し発展させ、client.htmlを作った。

--- server.mjs
+++ server.mjs
@@ -2,12 +2,18 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+var comments = '';
+
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
-    ws.send(data);
+    if (!data === 'plesecomments') {
+      comments=+data;
+    }
+    ws.send(comments);
+    
   });
 
 });
