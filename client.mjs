768d28548db52c63ec2ecfa047c8410d371e680f
DF64bit <96969533+DF64bit@users.noreply.github.com>
2023-07-01 23:14:38 +0000

    何したか覚えてないけどとりあえず試作品が完成!

--- client.mjs
+++ client.mjs
@@ -1,16 +1,19 @@
-const ws = new WebSocket('wss://df64bit-automatic-space-tribble-6x964q96qr7hx676-8080.preview.app.github.dev');
+const ws = new WebSocket('wss://df64bit-automatic-space-tribble-6x964q96qr7hx676-8080.preview.app.github.dev/');
 
 ws.onerror = ()=>{ console.error() };
 
 ws.onopen = function open() {
-  ws.send('something');
-};
-
-ws.onmessage = function message(data) {
-  console.log('received: %s', data);
+  ws.onmessage = function message(data) {
+  console.log(data);
+  var ms = ('%s', data)
+  console.log(data.data);
+  document.getElementById('commentsarea').innerHTML = data.data;
 };
 
 document.getElementById('send').addEventListener('click',
 (e)=>{
   ws.send(document.getElementById('inputtext').value);
 });
+
+};
+
--- server.mjs
+++ server.mjs
@@ -3,16 +3,24 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 
 var comments = '';
+var wt = [];
+function sendtoall(ms) {
+  for (let index = 0; index < wt.length; index++) {
+    wt[index].send(ms);
+    
+  }
+}
 
 wss.on('connection', function connection(ws) {
+  wt.push(ws);
+  ws.send('server connected.')
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    if (!data === 'plesecomments') {
-      comments=+data;
-    }
-    ws.send(comments);
+    console.log(data);
+    comments = comments + (data) + '<br>';
+    console.log(comments)
+    sendtoall(comments);
     
   });
 
